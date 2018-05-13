e10372c88ec695dbd0baca1f5855f237a5fc4a59
tomxzzhou <tomxzzhou@tencent.com>
2018-05-13 17:35:46 +0800

    使用handlebar渲染模板

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,45 @@
 const fs = require("fs");
 
-const fn_index = async (ctx) => {
-    ctx.response.body = fs.createReadStream('./public/form.html');
-}
 const fn_signin = async (ctx) => {
-    // let user = ctx.request.body.user || 'koa';
-    // let pass = ctx.request.body.pass || '123';
-    let {user='koa', pass="123"} = ctx.request.body;
-    console.log(`signin with name: ${user}, password: ${pass}`);
-    ctx.response.body = `welcome ${user}`;
+    let {email='koa', password="123"} = ctx.request.body;
+    console.log(`signin with name: ${email}, password: ${password}`);
+    // ctx.response.body = `welcome ${user}`;
+    if(email === '1173425790@qq.com' && password === '123456'){
+        console.log("signin ok!");
+        await ctx.render('signin-ok.html', {
+            title: 'Sign In Ok',
+            name: email
+        })
+    }else{
+        console.log("signin failed!");
+        await ctx.render('signin-failed.html', {
+            title: 'Sign In Failed.'
+        })
+    }
+}
+const fn_upload = async (ctx) => {
+    const body = ctx.request.body;
+    console.log("> uploadFile", body.files);
+    const tmpdir = os.tmpdir();
+    console.log("tmpdir:", tmpdir);
+    const filePaths = [];
+    const files = ctx.request.body.files || {};
+    console.log("keys:", Object.keys(files));
+    for(let key in files){
+        const file = files[key];
+        console.log("debug: ", key, file);
+        const reader = await fs.createReadStream(file.path);
+        const writer = fs.createWriteStream(`./upload/${Date.now()}_file.name`);
+        reader.pipe(writer);
+
+    }
+    ctx.body = '上传完成';
 }
 
 module.exports = {
-    "GET /": fn_index,
-    "POST /signin": fn_signin
+    "GET /": async (ctx) => {
+        await ctx.render("index.html", {});
+    },
+    "POST /signin": fn_signin,
+    "POST /upload": fn_upload
 }
--- index.js
+++ index.js
@@ -3,46 +3,47 @@ const fs = require("fs");
 const path = require("path");
 const os = require("os");
 const koaBody = require("koa-body");
-let staticFiles = require('./static-files'); 
-//也可以用koa-static实现: const static = require("koa-static"); app.use(static(__dirname, '/static'));
+const views = require("koa-views");
 let routerController = require("./controller");
 
 const app =new koa();
 
+const isProduction = process.env.NODE_ENV === 'production';
 
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    var start = new Date().getTime(), execTime;
+    await next();
+    execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}`);
+})
 
-const main = (ctx, next) => {
-    ctx.response.type = 'html';
-    console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`);
-    next();
+// static file support:
+if(!isProduction){
+    let staticFiles = require('./static-files'); //也可以用koa-static实现: const static = require("koa-static"); app.use(static(__dirname, '/static'));
+    app.use(staticFiles('/static/', __dirname+'/static'));
 }
 
-const uploadFile = async ctx => {
-    const body = ctx.request.body;
-    console.log("> uploadFile", body.files);
-    const tmpdir = os.tmpdir();
-    console.log("tmpdir:", tmpdir);
-    const filePaths = [];
-    const files = ctx.request.body.files || {};
-    console.log("keys:", Object.keys(files));
-    for(let key in files){
-        const file = files[key];
-        console.log("debug: ", key, file);
-        const reader = await fs.createReadStream(file.path);
-        const writer = fs.createWriteStream(`./upload/${Date.now()}_file.name`);
-        reader.pipe(writer);
-
-    }
-    ctx.body = '上传完成';
-    
-}
-
-app.use(staticFiles('/static/', __dirname+'/static'));
+// parse request body
 app.use(koaBody({multipart:true}));
-app.use(main);
-// app.use(route.post('/upload', uploadFile))
+
+// add nunjucks as view:
+// app.use(templating('views', {
+//     noCache: !isProduction,
+//     watch: !isProduction
+// }));
+var handlebars = require('handlebars');
+var layouts = require('handlebars-layouts');
+// Register helpers 
+handlebars.registerHelper(layouts(handlebars));
+// Register partials 
+handlebars.registerPartial('layout', fs.readFileSync(__dirname + '/views/base.hbs', 'utf8'));
+
+app.use(views(path.join(__dirname, 'views'), {map: {html: 'handlebars'}}));
+
+// add controller:
 app.use(routerController());
 
-
 app.listen(4000);
-console.log(`Server run on port: 4000`);
+console.log(`Server run on port: 4000...`);
